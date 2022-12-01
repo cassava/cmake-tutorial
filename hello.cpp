1e571e064b116001e340e35f441ac2d0019435e4
Ben Morgan <cassava@iexu.de>
2022-12-01 20:22:20 +0100

    Use std::optional from C++17

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <optional>
+#include <string>
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
+    std::optional<std::string> who{};
+
+    std::cout << "Hello " << (who ? *who : "world") << "!" << std::endl;
 
     return 0;
 }
