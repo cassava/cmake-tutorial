c894246032a497962a1dece5c17d50aa339d887f
Ben Morgan <cassava@iexu.de>
2022-12-01 20:51:22 +0100

    Use mylib in app/hello

--- app/hello.cpp
+++ app/hello.cpp
@@ -2,10 +2,14 @@
 #include <optional>
 #include <string>
 
+#include <mylib.hpp>
+
 int main() {
     std::optional<std::string> who{};
 
     std::cout << "Hello " << (who ? *who : "world") << "!" << std::endl;
+
+    std::cout << "Answer is " << mylib::get_answer() << "." << std::endl;
 
     return 0;
 }
